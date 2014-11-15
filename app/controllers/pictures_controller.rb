98eb46c575aaa1169fa9071e55824a050557ade5
Rafał Hirsz <rafal@hirsz.co>
2014-11-15 21:41:14 +0100

    Added caching for pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,6 +2,7 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.get(params[:id])
 
+    expires_in 2.days, public: true
     send_data @picture.image, type: 'image/jpeg', disposition: 'inline'
   end
 end
